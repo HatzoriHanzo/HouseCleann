e8d37992aea8b83693e02f323cb4dbca8c502d42
HatzoriHanzo <victorpierre155@gmail.com>
2019-11-24 15:26:24 -0300

    get updated Location Periodically ( video aula 5 ) by PiErRe_lOrD_of_PuSSy

--- app/src/main/java/com/example/houseclean/Diarista.java
+++
@@ -1,9 +0,0 @@
-package com.example.houseclean;
-
-public class Diarista {
-    public String nome;
-    public String cpf;
-    public String Endereco;
-    public Integer idade;
-    public String email;
-}
